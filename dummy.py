c1bd42d57e382ec1233b6e8f94e3763d0531457f
David Procházka <david@prochazka.dev>
2024-10-11 10:17:34 +0200

    `torch.concatenate` removed from dummy index

--- dummy.py
+++ dummy.py
@@ -2,17 +2,20 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING
 
-import numpy as np
 import torch
 from torch import Tensor
 
 from index import Index
 
 if TYPE_CHECKING:
+    import numpy as np
+
     from bucket import Bucket
 
 
 class DummyIndex(Index):
+    """A dummy index implementation that randomly stores the objects in the buckets."""
+
     def __init__(
         self,
         n_buckets: int,
@@ -28,35 +31,45 @@ class DummyIndex(Index):
         self.bucket_size = bucket_shape[0]
 
     def train(self, buckets: list[Bucket]) -> None:
-        # TODO: get rid of torch.concatenate
-        X, I = torch.concatenate([b.get_data() for b in buckets]), np.concatenate([b.get_ids() for b in buckets])
+        # Add the vectors to the new buckets
+        for existing_bucket in buckets:
+            bucket_data = existing_bucket.get_data()
+            bucket_indexes = existing_bucket.get_ids()
 
-        # Randomly assign vectors to buckets
-        classes = torch.randint(self.n_buckets, (X.shape[0],))
+            # Randomly assign vectors to buckets
+            classes = torch.randint(self.n_buckets, (existing_bucket.get_n_objects(),))
 
-        # Add the vectors to the buckets
-        for i, child_bucket in self.buckets.items():
-            child_bucket.insert(X[classes == i], I[classes == i])
+            for i, new_child_bucket in self.buckets.items():
+                new_child_bucket.insert(bucket_data[classes == i], bucket_indexes[classes == i])
 
         self.is_trained = True
 
-        # TODO: what if all objects were inserted into the same bucket? = unbalanced partitioning
-
     def insert(self, buckets: list[Bucket]) -> bool:
-        # TODO: get rid of torch.concatenate
-        X, I = torch.concatenate([b.get_data() for b in buckets]), np.concatenate([b.get_ids() for b in buckets])
+        total_n_objects = sum(b.get_n_objects() for b in buckets)
 
         # Predict to which bucket each vector belongs
-        bucket_ids = torch.randint(self.n_buckets, (X.shape[0],))
+        bucket_ids = torch.randint(self.n_buckets, (total_n_objects,))
 
         # Check that buckets do not overflow
         for i, n_objects_in_bucket in enumerate(torch.bincount(bucket_ids, minlength=self.n_buckets)):
             if n_objects_in_bucket + self.buckets[i].get_n_objects() > self.bucket_size:
                 return False  # Overflow detected
 
-        # Add the vectors to the buckets
-        for i, child_bucket in self.buckets.items():
-            child_bucket.insert(X[bucket_ids == i], I[bucket_ids == i])
+        # Add the vectors to the new buckets
+        offset = 0
+
+        for existing_bucket in buckets:
+            bucket_data = existing_bucket.get_data()
+            bucket_indexes = existing_bucket.get_ids()
+            relevant_bucket_ids = bucket_ids[offset : offset + existing_bucket.get_n_objects()]
+
+            for i, new_child_bucket in self.buckets.items():
+                new_child_bucket.insert(
+                    bucket_data[relevant_bucket_ids == i],
+                    bucket_indexes[relevant_bucket_ids == i],
+                )
+
+            offset += existing_bucket.get_n_objects()
 
         return True  # Insertion successful
 
