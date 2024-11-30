c52938cf5efea0dc20f77d7433d25c07f2ff0f05
David Procházka <david@prochazka.dev>
2024-11-30 08:06:44 +0100

    Run experiments on `elwe` cluster, added optimization

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #PBS -q elixircz@pbs-m1.metacentrum.cz
-#PBS -l select=1:ncpus=8:mem=1tb
+#PBS -l select=1:ncpus=8:mem=1tb:cluster=elwe
 #PBS -l walltime=96:00:00
 
 export OMP_NUM_THREADS=$PBS_NUM_PPN
@@ -11,5 +11,5 @@ module add mambaforge || exit 2
 mamba activate /storage/brno12-cerit/home/prochazka/.conda/envs/DynamicLearnedIndex || exit 3
 
 cd '/storage/brno12-cerit/home/prochazka/fi-lmi-data/personal/david/research/DynamicLearnedIndex' || exit 4
-python3 main.py --compaction-strategy='leveling' &>"/storage/brno12-cerit/home/prochazka/fi-lmi-data/personal/david/research/DynamicLearnedIndex/metacentrum-logs/run-${PBS_JOBID}.log"
+python3 -OO main.py --compaction-strategy='leveling' &>"/storage/brno12-cerit/home/prochazka/fi-lmi-data/personal/david/research/DynamicLearnedIndex/metacentrum-logs/run-${PBS_JOBID}.log"
 exit $?
