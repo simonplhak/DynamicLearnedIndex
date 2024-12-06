43a4691e8855889fab7397e2211446bbb9cd901c
David Procházka <david@prochazka.dev>
2024-12-06 09:07:27 +0100

    Improved log messages during search

--- config/search.py
+++ config/search.py
@@ -12,3 +12,6 @@ class SearchConfig:
     def __post_init__(self) -> None:
         assert self.k > 0
         assert self.nprobe > 0
+
+    def __str__(self) -> str:
+        return f'SearchConfig(strategy={self.search_strategy.__name__}, k={self.k}, nprobe={self.nprobe})'
