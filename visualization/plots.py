354c04e1993280cd5b29e7207bf0ba05b6f3d101
David Procházka <david@prochazka.dev>
2024-12-23 11:36:42 +0100

    Refactor plotting functions to use a unified line plot creator

--- visualization/plots.py
+++ visualization/plots.py
@@ -32,37 +32,71 @@ def save_relevant_results_to_csv(
     return df
 
 
-def plot_recall_vs_nprobe(df: pd.DataFrame, experiment_dir: Path) -> None:
-    plt = sns.lineplot(x='nprobe', y='avg_recall', hue='search_strategy', data=df)
-    plt.set_title('Number of visited buckets vs. average recall')
-    plt.set_xlabel('Number of visited buckets')
-    plt.set_ylabel('Average recall')
+def create_lineplot(
+    df: pd.DataFrame,
+    x: str,
+    y: str,
+    title: str,
+    xlabel: str,
+    ylabel: str,
+    output_path: Path,
+) -> None:
+    """Create and save a line plot using seaborn.
+
+    Args:
+        df: DataFrame containing the data
+        x: Column name for x-axis
+        y: Column name for y-axis
+        title: Plot title
+        xlabel: X-axis label
+        ylabel: Y-axis label
+        output_path: Path to save the plot
+
+    """
+    plt = sns.lineplot(x=x, y=y, hue='search_strategy', data=df)
+    plt.set_title(title)
+    plt.set_xlabel(xlabel)
+    plt.set_ylabel(ylabel)
     fig = plt.get_figure()
     assert fig
-    fig.savefig(experiment_dir / 'recall_vs_nprobe.pdf')
+    fig.savefig(output_path)
     fig.clear()
+
+
+def plot_recall_vs_nprobe(df: pd.DataFrame, experiment_dir: Path) -> None:
+    create_lineplot(
+        df=df,
+        x='nprobe',
+        y='avg_recall',
+        title='Number of visited buckets vs. average recall',
+        xlabel='Number of visited buckets',
+        ylabel='Average recall',
+        output_path=experiment_dir / 'recall_vs_nprobe.pdf',
+    )
 
 
 def plot_recall_vs_avg_time_per_query(df: pd.DataFrame, experiment_dir: Path) -> None:
-    plt = sns.lineplot(x='nprobe', y='avg_time_per_query', hue='search_strategy', data=df)
-    plt.set_title('Number of visited buckets vs. average time per query')
-    plt.set_xlabel('Number of visited buckets')
-    plt.set_ylabel('Average time per query')
-    fig = plt.get_figure()
-    assert fig
-    fig.savefig(experiment_dir / 'queries_per_second_vs_recall.pdf')
-    fig.clear()
+    create_lineplot(
+        df=df,
+        x='nprobe',
+        y='avg_time_per_query',
+        title='Number of visited buckets vs. average time per query',
+        xlabel='Number of visited buckets',
+        ylabel='Average time per query',
+        output_path=experiment_dir / 'queries_per_second_vs_recall.pdf',
+    )
 
 
 def plot_queries_per_second_vs_recall(df: pd.DataFrame, experiment_dir: Path) -> None:
-    plt = sns.lineplot(x='avg_recall', y='queries_per_second', hue='search_strategy', data=df)
-    plt.set_title('Average recall vs. queries per second')
-    plt.set_xlabel('Average recall')
-    plt.set_ylabel('Queries per second')
-    fig = plt.get_figure()
-    assert fig
-    fig.savefig(experiment_dir / 'recall_vs_avg_time_per_query.pdf')
-    fig.clear()
+    create_lineplot(
+        df=df,
+        x='avg_recall',
+        y='queries_per_second',
+        title='Average recall vs. queries per second',
+        xlabel='Average recall',
+        ylabel='Queries per second',
+        output_path=experiment_dir / 'recall_vs_avg_time_per_query.pdf',
+    )
 
 
 # TODO: histogram of time per query
