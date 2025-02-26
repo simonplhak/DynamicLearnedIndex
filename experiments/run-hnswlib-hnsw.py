35592bea49a8cd32d27a489fe179e7823eadca03
David Procházka <david@prochazka.dev>
2025-02-26 14:56:02 +0100

    Added script for running `hnswlib`'s HNSW implementation on metacentrum

--- experiments/run-hnswlib-hnsw.py
+++ experiments/run-hnswlib-hnsw.py
@@ -8,7 +8,6 @@ from execution_environment import detect_environment
 from hnswlib import Index
 from loguru import logger
 from torch import Tensor, empty, float32, manual_seed
-from tqdm import tqdm
 
 from dli.utils import (
     measure_memory_usage,
@@ -37,7 +36,7 @@ X = X.to(float32)
 def create_index(X: Tensor, ef_construction: int, M: int) -> Index:
     index = Index(space='ip', dim=X.shape[1])  # possible options are l2, cosine or ip
     index.init_index(max_elements=len(X), ef_construction=ef_construction, M=M)
-    for i in tqdm(range(len(X))):
+    for i in range(len(X)):
         index.add_items(X[i], i)
     return index
 
---
+++ experiments/scripts/run-hnswlib-hnsw-32CPUs-100M.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#PBS -q elixircz@pbs-m1.metacentrum.cz
+#PBS -l select=1:ncpus=32:mem=1200gb:cluster=elwe
+#PBS -l walltime=96:00:00
+#PBS -o /storage/brno12-cerit/home/prochazka/projects/DynamicLearnedIndex/experiments/metacentrum-logs
+#PBS -e /storage/brno12-cerit/home/prochazka/projects/DynamicLearnedIndex/experiments/metacentrum-logs
+#PBS -m ae
+#PBS -M davidprochazka@mail.muni.cz
+
+export OMP_NUM_THREADS=$PBS_NUM_PPN
+
+module add mambaforge || exit 2
+mamba activate /storage/brno12-cerit/home/prochazka/projects/DynamicLearnedIndex/env || exit 3
+
+cd '/storage/brno12-cerit/home/prochazka/projects/DynamicLearnedIndex' || exit 4
+python3 experiments/run-hnswlib-hnsw.py \
+    --compaction-strategy='leveling' \
+    --shrink-buckets-during-compaction \
+    --dataset-identifier='100M' \
+    &>"/storage/brno12-cerit/home/prochazka/projects/DynamicLearnedIndex/experiments/metacentrum-logs/${PBS_JOBID}.LOG"
+exit $?
