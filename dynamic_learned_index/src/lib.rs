1e1f8baea7711a5e4fc2327c8643ad3d8228697f
Simon Plhak <plhak.s@gmail.com>
2025-06-08 08:55:42 +0200

    refactor: clean lib

--- dynamic_learned_index/src/lib.rs
+++ dynamic_learned_index/src/lib.rs
@@ -1,6 +1,3 @@
-// use faiss::index_factory;
-use tch::Tensor;
-
 pub(crate) mod bucket;
 mod clustering;
 mod config;
@@ -12,28 +9,3 @@ pub mod types;
 mod util;
 pub use index::{Index, IndexConfig};
 pub use types::Id;
-
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
-
-pub fn print_tensor() {
-    let t = Tensor::from_slice(&[3, 1, 4, 1, 5]);
-    let t = t * 2;
-    t.print();
-}
-
-pub fn test() {
-    // let knn_index = index_factory(d, description, metric)
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
