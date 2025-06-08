9f86d4bed5b5545b73b489eaab7a104636870302
Simon Plhak <plhak.s@gmail.com>
2025-06-08 23:27:32 +0200

    feat: kick-off python linking

--- py_dynamic_learned_index/src/lib.rs
+++ py_dynamic_learned_index/src/lib.rs
@@ -1,20 +1,41 @@
 use pyo3::prelude::*;
 
-#[pyfunction]
-fn add(left: u64, right: u64) -> PyResult<u64> {
-    Ok(dynamic_learned_index::add(left, right))
+#[pyclass(unsendable)]
+struct DynamicLearnedIndex {
+    index: dynamic_learned_index::Index,
 }
 
-#[pyfunction]
-fn print_tensor() -> PyResult<u64> {
-    dynamic_learned_index::print_tensor();
-    Ok(1)
+#[pymethods]
+impl DynamicLearnedIndex {
+    #[new]
+    fn new() -> Self {
+        let index = dynamic_learned_index::IndexConfig::default()
+            .build()
+            .unwrap();
+        DynamicLearnedIndex { index }
+    }
+
+    fn search(&self, query: Vec<f32>, k: usize) -> PyResult<Vec<u32>> {
+        Ok(self.index.search(&query, k))
+    }
+
+    fn __repr__(&self) -> String {
+        "DynamicLearnedIndex()".to_string()
+    }
+
+    #[staticmethod]
+    fn default() -> Self {
+        DynamicLearnedIndex {
+            index: dynamic_learned_index::IndexConfig::default()
+                .build()
+                .unwrap(),
+        }
+    }
 }
 
 #[pymodule(crate = "pyo3")]
 fn py_dynamic_learned_index(m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add("__version__", env!("CARGO_PKG_VERSION"))?;
-    m.add_function(wrap_pyfunction!(add, m)?)?;
-    m.add_function(wrap_pyfunction!(print_tensor, m)?)?;
+    m.add_class::<DynamicLearnedIndex>()?;
     Ok(())
 }
