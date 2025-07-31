95f10c7a1edf188eb6d6f0adeb01c4d43204e392
Simon Plhak <plhak.s@gmail.com>
2025-07-31 11:04:53 +0200

    fix: use sort ustable in predictions sort - performance boost

--- dynamic_learned_index/src/model.rs
+++ dynamic_learned_index/src/model.rs
@@ -156,7 +156,7 @@ impl Model {
         let xs = vec2tensor(xs).to_device(self.device);
         let predictions = tensor2vec(&self.model.forward(&xs));
         let mut predictions = predictions.into_iter().enumerate().collect::<Vec<_>>();
-        predictions.sort_by(|a, b| b.1.partial_cmp(&a.1).unwrap());
+        predictions.sort_unstable_by(|a, b| b.1.partial_cmp(&a.1).unwrap());
         assert!(predictions.len() <= self.labels as usize);
         predictions
     }
