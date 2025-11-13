b53571a8451128376e6f3392d34852390a4db60e
Simon Plhak <plhak.s@gmail.com>
2025-11-13 21:05:12 +0100

    feat: add delete method to python example

--- py_dynamic_learned_index/example.py
+++ py_dynamic_learned_index/example.py
@@ -12,6 +12,7 @@ builder = (
         .distance_fn('dot')  # options: dot|l2
         .arity(3)  # arity of the tree structure created by the index
         .compaction_strategy('bentley_saxe:no_rebuild')  # type of levelling used to construct new levels of tree;
+        .delete_method('oid_to_bucket')
 )
 # options for compaction strategy: 
 #   compaction strategy types: bentley_saxe
