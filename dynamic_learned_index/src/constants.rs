388d594d99881d7242115eaf2c272137f73b7079
Šimon Plhák <plhak.s@gmail.com>
2026-03-13 14:57:15 +0100

    refactor: removed unused constants

--- dynamic_learned_index/src/constants.rs
+++ dynamic_learned_index/src/constants.rs
@@ -1,5 +1,3 @@
-const SIMD_REGISTER_SIZE: usize = 256;
-pub const LANES: usize = SIMD_REGISTER_SIZE / 32;
 pub const DEFAULT_BUCKET_SIZE: usize = 5_000;
 pub const DEFAULT_BUFFER_SIZE: usize = 5_000;
 pub const DEFAULT_INPUT_SHAPE: usize = 768;
