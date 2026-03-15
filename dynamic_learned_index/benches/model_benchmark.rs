a420cdb1e57fdbb6fe96a685f9f0dcf76f83dcc6
Šimon Plhák <plhak.s@gmail.com>
2026-03-15 17:04:30 +0100

    fix: simplify model benchmarks

--- dynamic_learned_index/benches/model_benchmark.rs
+++ dynamic_learned_index/benches/model_benchmark.rs
@@ -2,12 +2,8 @@
 
 extern crate test;
 
-#[cfg(feature = "candle")]
-use dynamic_learned_index::model::{candle_model::Model as CandleModel, CandleBackend};
-#[cfg(feature = "tch")]
-use dynamic_learned_index::model::{tch_model::Model as TchModel, TchBackend};
 use dynamic_learned_index::model::{
-    BaseModelBuilder, ModelInterface, ModelLayer, RetrainStrategy, TrainParams,
+    Model, ModelBuilder, ModelInterface, ModelLayer, RetrainStrategy, TrainParams,
 };
 use dynamic_learned_index::structs::LabelMethod;
 use dynamic_learned_index::ModelDevice;
@@ -20,7 +16,7 @@ const NUM_CLUSTERS: usize = 9;
 const BATCH_SIZE: usize = 256;
 const HIDDEN_NEURONS: usize = 256;
 const EPOCHS: usize = 1;
-const PREDICT_MANY_SIZE: usize = 10_000;
+const PREDICT_MANY_SIZE: usize = 1_000;
 
 fn generate_random_data(size: usize, dim: usize) -> Vec<f32> {
     let mut rng = rand::rng();
@@ -29,9 +25,8 @@ fn generate_random_data(size: usize, dim: usize) -> Vec<f32> {
         .collect()
 }
 
-#[cfg(feature = "tch")]
-fn pytorch_model() -> TchModel {
-    BaseModelBuilder::<TchBackend>::default()
+fn model() -> Model {
+    ModelBuilder::default()
         .device(ModelDevice::Cpu)
         .input_nodes(VECTOR_DIM as i64)
         .add_layer(ModelLayer::Linear(HIDDEN_NEURONS))
@@ -50,84 +45,20 @@ fn pytorch_model() -> TchModel {
         .unwrap()
 }
 
-#[cfg(feature = "tch")]
 #[bench]
-fn bench_pytorch_model_train(b: &mut Bencher) {
+fn bench_model_train(b: &mut Bencher) {
     let data = generate_random_data(SAMPLE_SIZE, VECTOR_DIM);
 
     b.iter(|| {
-        let mut model = pytorch_model();
-        model.train(&data);
-    });
-}
-
-#[cfg(feature = "tch")]
-#[bench]
-fn bench_pytorch_model_predict_single(b: &mut Bencher) {
-    let train_data = generate_random_data(1000, VECTOR_DIM); // Smaller training set
-    let test_vector = generate_random_data(1, VECTOR_DIM);
-
-    let mut model = pytorch_model();
-
-    model.train(&train_data);
-
-    b.iter(|| {
-        let _result = model.predict(&test_vector);
-    });
-}
-
-#[cfg(feature = "tch")]
-#[bench]
-fn bench_pytorch_model_predict_many(b: &mut Bencher) {
-    let train_data = generate_random_data(1000, VECTOR_DIM);
-    let test_data = generate_random_data(PREDICT_MANY_SIZE, VECTOR_DIM); // 100 test vectors
-
-    let mut model = pytorch_model();
-
-    model.train(&train_data);
-
-    b.iter(|| {
-        let _result = model.predict_many(&test_data);
-    });
-}
-
-#[cfg(feature = "candle")]
-fn candle_model() -> CandleModel {
-    BaseModelBuilder::<CandleBackend>::default()
-        .device(ModelDevice::Cpu)
-        .input_nodes(VECTOR_DIM as i64)
-        .add_layer(ModelLayer::Linear(HIDDEN_NEURONS))
-        .add_layer(ModelLayer::ReLU)
-        .add_layer(ModelLayer::Linear(HIDDEN_NEURONS))
-        .labels(NUM_CLUSTERS)
-        .train_params(TrainParams {
-            threshold_samples: SAMPLE_SIZE,
-            batch_size: BATCH_SIZE,
-            epochs: EPOCHS,
-            max_iters: 10,
-            retrain_strategy: RetrainStrategy::NoRetrain,
-        })
-        .label_method(LabelMethod::KMeans)
-        .build()
-        .unwrap()
-}
-
-#[cfg(feature = "candle")]
-#[bench]
-fn bench_candle_model_train(b: &mut Bencher) {
-    let data = generate_random_data(SAMPLE_SIZE, VECTOR_DIM);
-
-    b.iter(|| {
-        let mut model = candle_model();
+        let mut model = model();
         model.train(&data).unwrap();
     });
 }
 
-#[cfg(feature = "candle")]
 #[bench]
-fn bench_candle_model_predict_single(b: &mut Bencher) {
-    let train_data = generate_random_data(1000, VECTOR_DIM);
-    let mut model = candle_model();
+fn bench_model_predict_single(b: &mut Bencher) {
+    let train_data = generate_random_data(SAMPLE_SIZE, VECTOR_DIM);
+    let mut model = model();
     let test_vector = model
         .vec2tensor(&generate_random_data(1, VECTOR_DIM))
         .unwrap();
@@ -139,13 +70,12 @@ fn bench_candle_model_predict_single(b: &mut Bencher) {
     });
 }
 
-#[cfg(feature = "candle")]
 #[bench]
-fn bench_candle_model_predict_many(b: &mut Bencher) {
-    let train_data = generate_random_data(1000, VECTOR_DIM);
+fn bench_model_predict_many(b: &mut Bencher) {
+    let train_data = generate_random_data(SAMPLE_SIZE, VECTOR_DIM);
     let test_data = generate_random_data(PREDICT_MANY_SIZE, VECTOR_DIM);
 
-    let mut model = candle_model();
+    let mut model = model();
 
     model.train(&train_data).unwrap();
 
