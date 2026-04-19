60a6fcd8626682f9b093dfe0dfb988b1e3ecf187
Šimon Plhák <plhak.s@gmail.com>
2026-04-19 14:41:48 +0200

    feat: reintroduced mix models architecture

---
+++ dynamic_learned_index/src/model/mix_model.rs
@@ -0,0 +1,107 @@
+use std::path::PathBuf;
+
+use crate::model::candle_model;
+use crate::model::tch_model;
+use crate::model::BaseModelBuilder;
+use crate::model::CandleBackend;
+use crate::model::ModelInterface as _;
+use crate::model::TchBackend;
+use crate::DliError;
+use candle_core::Tensor;
+use rand::Rng as _;
+
+use crate::{model::MixBackend, ArraySlice, DliResult, ModelConfig};
+
+impl crate::model::BaseModelBuilder<MixBackend> {
+    pub fn build(&self) -> DliResult<Model> {
+        let device = self.device.ok_or(DliError::MissingAttribute("device"))?;
+        let input_nodes = self
+            .input_nodes
+            .ok_or(DliError::MissingAttribute("input_nodes"))?;
+        let labels = self.labels.ok_or(DliError::MissingAttribute("labels"))?;
+        let train_params = self.train_params.unwrap_or_default();
+        let label_method = self
+            .label_method
+            .ok_or(DliError::MissingAttribute("label_method"))?;
+        let mut tch_builder = BaseModelBuilder::<TchBackend>::default();
+        tch_builder
+            .device(device)
+            .input_nodes(input_nodes)
+            .labels(labels)
+            .train_params(train_params)
+            .label_method(label_method)
+            .layers(self.layers.clone());
+        let mut candle_builder = BaseModelBuilder::<CandleBackend>::default();
+        candle_builder
+            .device(device)
+            .input_nodes(input_nodes)
+            .labels(labels)
+            .train_params(train_params)
+            .label_method(label_method)
+            .layers(self.layers.clone());
+        if let Some(weights_path) = self.weights_path.clone() {
+            tch_builder.weights_path(weights_path.clone());
+        }
+
+        let mut model = Model {
+            tch_model: tch_builder.build()?,
+            candle_model: candle_builder.build()?,
+            candle_builder,
+            input_shape: input_nodes as usize,
+        };
+        model.sync_weights_from_tch_to_candle()?;
+        Ok(model)
+    }
+}
+
+pub struct Model {
+    tch_model: tch_model::Model,
+    candle_model: candle_model::Model,
+    candle_builder: BaseModelBuilder<CandleBackend>,
+    pub input_shape: usize,
+}
+
+impl crate::model::ModelInterface for Model {
+    type TensorType = Tensor;
+
+    fn predict(&self, xs: &Self::TensorType) -> DliResult<Vec<(usize, f32)>> {
+        self.candle_model.predict(xs)
+    }
+
+    fn predict_many(&self, xs: &ArraySlice) -> DliResult<Vec<usize>> {
+        self.candle_model.predict_many(xs)
+    }
+
+    fn train(&mut self, xs: &ArraySlice) -> DliResult<()> {
+        self.tch_model.train(xs)?;
+        self.sync_weights_from_tch_to_candle()
+    }
+
+    fn retrain(&mut self, xs: &ArraySlice) -> DliResult<()> {
+        self.tch_model.retrain(xs)?;
+        self.sync_weights_from_tch_to_candle()
+    }
+
+    fn dump(&self, weights_filename: PathBuf) -> DliResult<ModelConfig> {
+        self.tch_model.dump(weights_filename)
+    }
+
+    fn memory_usage(&self) -> usize {
+        self.candle_model.memory_usage() + self.tch_model.memory_usage()
+    }
+
+    fn vec2tensor(&self, xs: &[f32]) -> DliResult<Self::TensorType> {
+        self.candle_model.vec2tensor(xs)
+    }
+}
+
+impl Model {
+    pub fn sync_weights_from_tch_to_candle(&mut self) -> DliResult<()> {
+        let random_id = rand::rng().random::<u64>();
+        let weights_path = std::path::PathBuf::from("/tmp")
+            .join(format!("mix_model_weights_{}.safetensors", random_id));
+        self.tch_model.dump(weights_path.clone())?;
+        self.candle_model = self.candle_builder.weights_path(weights_path).build()?;
+        Ok(())
+    }
+}
